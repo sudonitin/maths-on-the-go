8b8c3418b112a0f20711812d4fa9744fe288b783
feministAF <santiagoreha24@gmail.com>
2019-09-20 17:34:12 +0530

    registration done

--- server/config/key.js
+++ server/config/key.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://Reha:Reha@cluster0-kqj2r.mongodb.net/test?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://Reha:Reha@cluster0-kqj2r.mongodb.net/test?retryWrites=true&w=majority",
+    secret:"secret"
   };
