5cd82db023df31a12b531813863129905b3181a6
feministAF <santiagoreha24@gmail.com>
2019-09-30 00:48:34 +0530

    basic layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
+import { Router,Switch,Route } from 'react-router-dom';
+import Navbar from '../src/components/layout/Navbar';
+import Landing from '../src/components/layout/Landing'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+      
+      <div className="app">
+        <Navbar />
+      </div>
+      
+    )
+  }
 }
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Navbar extends Component {
+  render() {
+    return (
+      
+      <div>
+          <h1>hello</h1>
+      </div>
+
+    );
+  }
+}export default Navbar;
