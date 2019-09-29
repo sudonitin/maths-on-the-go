82c9b47d2618af2d13182981e51d32bec5331ad8
globefire <sahun5774@gmail.com>
2019-09-29 20:34:17 +0530

    updatejs request changed to post

--- server/routes/updatescore/update.js
+++ server/routes/updatescore/update.js
@@ -6,10 +6,8 @@ const User = require('../../models/User');
 const {users,levels} = require('../../connections/connections');
 
 
-router.put('/upscore', token.auth, (req,res)=> {
-    // const level = req.param.level;
-    //console.log(req.params.email);
-    //console.log(req.params.level);
+router.post('/upscore', token.auth, (req,res)=> {
+
     users.collection('users').findOneAndUpdate({ email: req.body.email }, {$set:{[req.body.level]:req.body.score}}, {returnOriginal:false}, (err,user) => {
         if(err) return res.json(err);
         else res.json({user});
