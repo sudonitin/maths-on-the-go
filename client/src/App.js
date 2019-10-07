d5241dfee7ed11dfc27ed599108a4c9556bb5e97
globefire <sahun5774@gmail.com>
2019-10-07 20:59:40 +0530

    file structure went wrong

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from './components/HomePage';
-import Footer from "./components/layout/Footer";
+import Footer from "./components/footer/Footer";
 import Navbar from "./components/layout/Navbar";
 import Register from "./components/sign_in_out/Register";
 import Login from "./components/sign_in_out/Login";
-import Graph from "./components/layout/Graph";
+import Graph from "./components/dashboard/Graph";
 
 class App extends Component {
   render() {
@@ -15,7 +15,7 @@ class App extends Component {
           <Route exact path="/" component={HomePage}/>
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
-          <Route exact path="/score" component={Graph} />
+          <Route exact path="/dashboard" component={Graph} />
           <Footer />
         </div>
       </Router>
